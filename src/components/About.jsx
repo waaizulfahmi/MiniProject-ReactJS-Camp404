e0adec2e4b44f8a66ace8323ed2bbdd96029a4ce
waaizulfahmi <wandaaizulf@gmail.com>
2023-02-25 18:35:20 +0700

    v2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 function About(){
     return(
-        <div>
-            <h1>Ini About</h1>
+        <div className="card shadow mt-5 mx-5">
+            <div className="row px-3 py-3">
+                <div className="col-6">
+                    <h1>About Us</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus amet laboriosam.</p>
+                </div>
+                <div className="col-6">
+                    <h1>sdasd</h1>
+                </div>
+
+            </div>
+            
         </div>
     )
 }
