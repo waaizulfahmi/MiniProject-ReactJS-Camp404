24be4023f9d780f4cc1727e778dfd1eac0b17e03
waaizulfahmi <wandaaizulf@gmail.com>
2023-02-26 21:31:56 +0700

    update beranda

--- src/components/Beranda.jsx
+++ src/components/Beranda.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import sepatuGambar from "./images/sepatu.jpg";
 
 function Beranda(){
     return(
-        <div>
-            <h1>Hello World</h1>
+        <div className="container-fluid px-0">
+            <img className="img-fluid" src={sepatuGambar} alt="" width="auto" height="auto"  srcset="" />
+            <h1 className="text-center centered">Welcome !</h1>    
+            <h1 className="text-center centered2">Nike Official Store</h1> 
+            <button className="btn btn-primary centered3">Belanja Sekarang !</button>   
+            {/* <h1>skdaksdakd</h1> */}
         </div>
 
-    );
+    )
 }
 
 export default Beranda;
