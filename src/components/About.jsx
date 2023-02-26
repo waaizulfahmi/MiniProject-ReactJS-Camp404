c99574b12141b1bab31a7d5ffbbae6dcaeb0d5ca
waaizulfahmi <wandaaizulf@gmail.com>
2023-02-26 12:12:55 +0700

    update about & katalog

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import image from "./images/about us.jpg";
 
 function About(){
     return(
         <div className="card shadow mt-5 mx-5">
-            <div className="row px-3 py-3">
-                <div className="col-6">
+            <div className="row ">
+                <div className="col-6 px-5 py-5" >
                     <h1>About Us</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus amet laboriosam.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum, asperiores nesciunt dolore assumenda consequatur necessitatibus quibusdam officia similique illum repellat?</p>
                 </div>
                 <div className="col-6">
-                    <h1>sdasd</h1>
+                    <img src={image} alt="" srcset="" width="623px" className="rounded"/>
                 </div>
 
             </div>
--- src/components/Katalog.jsx
+++ src/components/Katalog.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "./logo.png";
+import Logo from "./images/logo.png";
 
 function Katalog(){
     return(
