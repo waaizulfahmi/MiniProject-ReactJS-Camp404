25093df7ce7e73cfcac274901fee83398cc97cd5
waaizulfahmi <wandaaizulf@gmail.com>
2023-03-01 21:55:43 +0700

    update katalog 1

--- src/components/Katalog.jsx
+++ src/components/Katalog.jsx
@@ -1,63 +1,41 @@
-import React from "react";
-import Logo from "./images/logo.png";
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import logo from "./images/logo.png";
+import axios from "axios";
 
 function Katalog() {
+  const [shoes, setShoes] = useState([]);
+
+  useEffect(() => {
+    retrieveData();
+  }, []);
+
+  function retrieveData() {
+    axios
+      .get("http://localhost:4000/shoes")
+      .then((response) => {
+        setShoes(response.data);
+      })
+      .catch(function (error) {
+        console.log(error.response.data);
+      });
+  }
+
   return (
-    <div className="card shadow mt-5 mx-5 px-5 py-5 ">
-      <h3 className="text-center pt-4 pb-5">Katalog Produk </h3>
-      <div className="row">
-        <div className="col-3">
-          <div className="card">
-            <div className="row">
-              <img src={Logo} alt="" srcset="" height="200px" width="200px" />
-            </div>
-            <div className="row">
-              <h2 className="mt-3 ms-2">Sepatu Indoor 2</h2>
-            </div>
-            <div className="row">
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="card">
-            <div className="row">
-              <img src={Logo} alt="" srcset="" height="200px" width="200px" />
-            </div>
-            <div className="row">
-              <h2 className="mt-3">Sepatu Indoor 2</h2>
-            </div>
-            <div className="row">
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="card">
-            <div className="row">
-              <img src={Logo} alt="" srcset="" height="200px" width="200px" />
-            </div>
-            <div className="row">
-              <h2 className="mt-3">Sepatu Indoor 2</h2>
-            </div>
-            <div className="row">
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="card">
-            <div className="row">
-              <img src={Logo} alt="" srcset="" height="200px" width="200px" />
-            </div>
-            <div className="row">
-              <h2 className="mt-3">Sepatu Indoor 2</h2>
-            </div>
-            <div className="row">
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </div>
-        </div>
+    <div className=" mt-5 mx-5 px-5 py-1 ">
+      <h3 className="text-center pb-5">Katalog Produk </h3>
+      <div className="grid">
+        {shoes.map((sepatu, index) => (
+          <Card style={{ width: "18rem" }} key={index} className="box shadow">
+            <Card.Img variant="top" src={logo} />
+            <Card.Body>
+              <Card.Title className="text-black">{sepatu.nama}</Card.Title>
+              <Card.Text>Rp. {sepatu.harga.toLocaleString()}</Card.Text>
+              <Button variant="primary">Buy Now !</Button>
+            </Card.Body>
+          </Card>
+        ))}
       </div>
     </div>
   );
